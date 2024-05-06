81b7364e1c9dbce61cf20f89c77b17ce400fe2a5
sofiaaaa <145217327+saaaan25@users.noreply.github.com>
2024-05-06 16:30:40 -0500

    Update camposMedicos.js

--- datos/camposMedicos.js
+++ datos/camposMedicos.js
@@ -1,4 +1,4 @@
-import { medicos, citasMedicas } from '../datos/citaMedica.js'
+import { medicos} from '../datos/citaMedica.js'
 
 let camposMedicos = ['--Seleccionar especialidad--', 'Psiquiatría', 'Cardiología', 'Oftalmología', 'Ginecología', 'Traumatología', 'Gastroenterología', 'Urología', 'Obstetricia', 'Pediatría']
 let horasDisponibles = ['--Seleccionar hora--', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00']
