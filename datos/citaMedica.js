ae583725444c8ee5ed78a8c55a100587a9fd06e0
sofiaaaa <145217327+saaaan25@users.noreply.github.com>
2024-05-06 21:51:04 -0500

    Update citaMedica.js

--- datos/citaMedica.js
+++ datos/citaMedica.js
@@ -5,43 +5,76 @@
 let medico1 = {
     nombre: 'Ciro Rodriguez',
     especialidades: ['Psiquiatría', 'Cardiología', 'Oftalmología', 'Ginecología'],
-    horasOcupadas: [8, 10, 11, 16, 20]  // Formato 24 horas
+    horasOcupadas: ['08:00', '10:00', '11:00', '16:00', '20:00']  // Formato 24 horas
 }
 
 let medico2 = {
     nombre: 'Fanny Mori',
     especialidades: ['Gastroenterología', 'Ginecología', 'Obstetricia', 'Urología'],
-    horasOcupadas: [7, 9, 12, 15, 17]  // Formato 24 horas
+    horasOcupadas: ['07:00', '09:00', '12:00', '15:00', '17:00']  // Formato 24 horas
 }
 
 let medico3 = {
     nombre: 'Pablo Romero',
     especialidades: ['Cardiología', 'Traumatología', 'Gastroenterología', 'Urología'],
-    horasOcupadas: [9, 11, 15, 16, 18]  // Formato 24 horas
+    horasOcupadas: ['09:00', '11:00', '15:00', '16:00', '18:00']  // Formato 24 horas
 }
 
 let medico4 = {
     nombre: 'Maria Elena',
     especialidades: ['Psiquiatría', 'Oftalmología', 'Traumatología', 'Pediatría'],
-    horasOcupadas: [10, 13, 14, 19, 20]  // Formato 24 horas
+    horasOcupadas: ['10:00', '13:00', '14:00', '19:00', '20:00']  // Formato 24 horas
 }
 
 let medico5 = {
     nombre: 'Heddy Colca',
     especialidades: ['Obstetricia', 'Pediatría', 'Psiquiatría', 'Oftalmología'],
-    horasOcupadas: [10, 13, 14, 18, 19]  // Formato 24 horas
+    horasOcupadas: ['10:00', '13:00', '14:00', '18:00', '19:00']  // Formato 24 horas
 }
 
+let medicos = [medico1, medico2, medico3, medico4, medico5]
 
 //-------------------- Citas médicas --------------------//
 
 let citasMedicas = [
-    {especialidadCita: 'Obstetricia', dia: 4, mes: 5, anio: 2024, horaCita: 14, medicoCita: 'Heddy Colca', correo: 'harumi.contreras@unmsm.edu.pe', motivo: 'Chequeo periódico'},
-    {especialidadCita: 'Cardiología', dia: 5, mes: 5, anio: 2024, horaCita: 11, medicoCita: 'Ciro Rodriguez', correo: 'joar.aranda@unmsm.edu.pe', motivo: 'Dolor de pecho'}
+    {especialidadCita: 'Obstetricia', fecha: '2024-5-14', horaCita: 14, medicoCita: 'Heddy Colca', correo: 'harumi.contreras@unmsm.edu.pe', motivo: 'Chequeo periódico'},
+    {especialidadCita: 'Cardiología', fecha: '2024-5-10', horaCita: 11, medicoCita: 'Ciro Rodriguez', correo: 'joar.aranda@unmsm.edu.pe', motivo: 'Dolor de pecho'}
 ]
 
-let resultado = citasMedicas[1]
+function agregarCita(nuevaEspecialidad, nuevaFecha, nuevaHoraCita, nuevoMedicoCita, nuevoCorreo, nuevoMotivo) {
+    let nuevaCita = {
+        especialidades: nuevaEspecialidad,
+        fecha: nuevaFecha,
+        horaCita: nuevaHoraCita,
+        medicoCita: nuevoMedicoCita,
+        correo: nuevoCorreo,
+        motivo: nuevoMotivo,
+    }
+    citasMedicas.push(nuevaCita)
+    console.log(nuevaCita)
+    document.getElementById('mensaje').innerHTML = 'Su cita ha sido agendada'
+}
 
-console.log(resultado)
+let agendarC = document.getElementById('botonagendar')
 
-//array.push(elemento) para agregar citas
+export { medicos}
+
+//-------------------- Citas médicas --------------------//
+const form = document.getElementById('form')
+
+form.addEventListener("submit", e => {
+    e.preventDefault()
+    const elegidaESP = document.getElementById('escogerespecialidad')
+    const elegidaFEC = document.getElementById('fecha_cita')
+    const elegidoHOR = document.getElementById('escogerhora')
+    const elegidoMED = document.getElementById('escogermedico')
+    const elegidoCOR = document.getElementById('email')
+    const elegidoMOT = document.getElementById('especificaciones')
+    let nuevaEspecialidad = elegidaESP.innerHTML
+    let nuevaFecha = elegidaFEC.innerHTML
+    let nuevaHoraCita = elegidoHOR.innerHTML
+    let nuevoMedicoCita = elegidoMED.innerHTML
+    let nuevoCorreo = elegidoCOR.innerHTML
+    let nuevoMotivo = elegidoMOT.innerHTML
+    agregarCita(nuevaEspecialidad, nuevaFecha, nuevaHoraCita, nuevoMedicoCita, nuevoCorreo, nuevoMotivo)
+})
